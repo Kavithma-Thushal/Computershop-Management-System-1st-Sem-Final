748da1488bfc829bdf8114e203f2007bb6225cd0
Thushal2001 <kavithmathushal9007@gmail.com>
2023-03-25 13:28:52 +0530

    initial commit

--- src/main/java/lk/ijse/computershop/controller/CashierdashboardFormController.java
+++ src/main/java/lk/ijse/computershop/controller/CashierdashboardFormController.java
@@ -1,5 +1,10 @@
 package lk.ijse.computershop.controller;
 
+import javafx.event.ActionEvent;
+
 public class CashierdashboardFormController {
 
+    public void managecustomersOnAction(ActionEvent event) {
+
+    }
 }
