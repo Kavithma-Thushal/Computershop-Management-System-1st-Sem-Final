d8daf593fc83a5f9250c7f8cb6365cf469d056bf
Thushal2001 <kavithmathushal9007@gmail.com>
2023-03-25 10:33:40 +0530

    second commit

--- src/main/java/lk/ijse/computershop/controller/Main.java
+++ src/main/java/lk/ijse/computershop/controller/Main.java
@@ -3,5 +3,6 @@ package lk.ijse.computershop.controller;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("Hello World");
     }
 }
