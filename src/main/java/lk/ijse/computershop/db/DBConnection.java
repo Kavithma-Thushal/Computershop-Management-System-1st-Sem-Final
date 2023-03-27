a900f0be01ed4d505be39e2afe48345b1dc8841e
Thushal2001 <kavithmathushal9007@gmail.com>
2023-03-27 05:45:32 +0530

    initial commit

---
+++ src/main/java/lk/ijse/computershop/db/DBConnection.java
@@ -0,0 +1,27 @@
+package lk.ijse.computershop.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static DBConnection dbconnection;
+    private Connection connection;
+
+    private DBConnection() throws SQLException {
+        this.connection = DriverManager.getConnection("", "root", "1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        if (dbconnection == null) {
+            return new DBConnection();
+        } else {
+            return dbconnection;
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
