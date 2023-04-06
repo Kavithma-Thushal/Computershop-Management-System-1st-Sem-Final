ed71fe3d2e65cd515088fd583312278ab5a72868
Thushal2001 <kavithmathushal9007@gmail.com>
2023-04-06 17:01:11 +0530

    Initial commit

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -14,12 +15,11 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("view/login_form.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("view/splashscreen_form.fxml"));
         Scene scene = new Scene(parent);
         primaryStage.setScene(scene);
         primaryStage.centerOnScreen();
-        primaryStage.setTitle("login");
-        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 }
---
+++ src/main/java/lk/ijse/computershop/controller/SplashscreenFormController.java
@@ -0,0 +1,72 @@
+package lk.ijse.computershop.controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SplashscreenFormController {
+
+    @FXML
+    private AnchorPane splashScreen;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private ProgressIndicator progressIndicator;
+
+    public void initialize() {
+        new ShowSplashScreen().start();
+    }
+
+    class ShowSplashScreen extends Thread {
+        public void run() {
+            try {
+                for (int i = 0; i <= 10; i++) {
+                    double x = i * (0.1);
+                    progressBar.setProgress(x);
+                    progressIndicator.setProgress(x);
+
+                    if (i * 10 == 100) {
+                        progressIndicator.setVisible(true);
+                        progressIndicator.setProgress(1);
+                    }
+
+                    try {
+                        Thread.sleep(300);
+                    } catch (Exception e) {
+                        new Alert(Alert.AlertType.ERROR, "please try again...!").show();
+                    }
+                }
+
+                Platform.runLater(() -> {
+                    Stage stage = new Stage();
+                    Parent root = null;
+                    try {
+                        root = FXMLLoader.load(getClass().getResource("/view/login_form.fxml"));
+                    } catch (Exception e) {
+                        Logger.getLogger(SplashscreenFormController.class.getName()).log(Level.SEVERE, null, e);
+                    }
+
+                    assert root != null;
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.initStyle(StageStyle.UNDECORATED);
+                    stage.show();
+                    splashScreen.getScene().getWindow().hide();
+                });
+            } catch (Exception e) {
+                Logger.getLogger(SplashscreenFormController.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+}
