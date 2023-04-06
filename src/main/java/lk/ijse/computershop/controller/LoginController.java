7da650f655f502a24dc95dc53e48a9ff44698136
Thushal2001 <kavithmathushal9007@gmail.com>
2023-04-06 22:38:30 +0530

    Initial commit

--- src/main/java/lk/ijse/computershop/controller/LoginController.java
+++ src/main/java/lk/ijse/computershop/controller/LoginController.java
@@ -1,5 +1,10 @@
 package lk.ijse.computershop.controller;
 
+import javafx.scene.input.MouseEvent;
+
 public class LoginController {
 
+    public void closeOnAction(MouseEvent mouseEvent) {
+        System.exit(0);
+    }
 }
