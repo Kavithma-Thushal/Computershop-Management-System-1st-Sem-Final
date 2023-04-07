8ccce2d59d5d6ece54dba46abbf460dfa6a62e2c
Thushal2001 <kavithmathushal9007@gmail.com>
2023-04-08 01:28:06 +0530

    Initial commit

--- src/main/java/lk/ijse/computershop/controller/LoginFormController.java
+++ src/main/java/lk/ijse/computershop/controller/LoginFormController.java
@@ -1,19 +1,21 @@
 package lk.ijse.computershop.controller;
 
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 import lk.ijse.computershop.util.DateAndTime;
 
 import java.io.IOException;
@@ -23,11 +25,7 @@ import java.util.ResourceBundle;
 public class LoginFormController implements Initializable {
 
     @FXML
-    private AnchorPane root;
-    @FXML
-    private TextField txtUsername;
-    @FXML
-    private PasswordField txtPassword;
+    private AnchorPane rootLogin;
     @FXML
     private Label lblDate;
     @FXML
@@ -35,32 +33,55 @@ public class LoginFormController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        DateAndTime.loadDateAndTime(lblDate,lblTime);
+        DateAndTime.loadDateAndTime(lblDate, lblTime);
     }
 
     @FXML
     private void cashierLoginOnAction(MouseEvent mouseEvent) throws IOException {
-        if (txtUsername.getText().equals("cashier") && txtPassword.getText().equals("1234")) {
-            Parent parent = FXMLLoader.load(getClass().getResource("/view/cashierdashboard_form.fxml"));
-            Stage stage = (Stage) root.getScene().getWindow();
-            Scene scene = new Scene(parent);
-            stage.setScene(scene);
-            stage.centerOnScreen();
-        } else {
-            new Alert(Alert.AlertType.ERROR, "please try again...!").show();
-        }
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/cashier_form.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+
+        stage.setX(385);
+        stage.setY(263);
     }
 
     @FXML
     private void adminLoginOnAction(MouseEvent mouseEvent) throws IOException {
-        if (txtUsername.getText().equals("admin") && txtPassword.getText().equals("1234")) {
-            Parent parent = FXMLLoader.load(getClass().getResource("/view/admindashboard_form.fxml"));
-            Stage stage = (Stage) root.getScene().getWindow();
-            Scene scene = new Scene(parent);
-            stage.setScene(scene);
-            stage.centerOnScreen();
-        } else {
-            new Alert(Alert.AlertType.ERROR, "please try again...!").show();
+
+    }
+
+    @FXML
+    private void playMouseEnterAnimation(MouseEvent mouseEvent) {
+        if (mouseEvent.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) mouseEvent.getSource();
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.05);
+            scaleT.setToY(1.05);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+            glow.setColor(Color.BLACK);
+            glow.setWidth(20);
+            glow.setHeight(20);
+            glow.setRadius(20);
+            icon.setEffect(glow);
+        }
+    }
+
+    @FXML
+    private void playMouseExitAnimation(MouseEvent mouseEvent) {
+        if (mouseEvent.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) mouseEvent.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+            icon.setEffect(null);
         }
     }
 
