dd8695d689ba1fbc84214536037e1210366a5273
Thushal2001 <kavithmathushal9007@gmail.com>
2023-04-08 07:46:22 +0530

    Initial commit

--- src/main/java/lk/ijse/computershop/controller/LoginFormController.java
+++ src/main/java/lk/ijse/computershop/controller/LoginFormController.java
@@ -25,7 +25,7 @@ import java.util.ResourceBundle;
 public class LoginFormController implements Initializable {
 
     @FXML
-    private AnchorPane rootLogin;
+    private AnchorPane root;
     @FXML
     private Label lblDate;
     @FXML
--- src/main/java/lk/ijse/computershop/util/UILoader.java
+++ src/main/java/lk/ijse/computershop/util/UILoader.java
@@ -17,59 +17,24 @@ public class UILoader {
     }
 
     public static void loadUiDashBoard(AnchorPane anchorPane, String location) throws IOException {
-
-        /**----------------------------------FULL CODE------------------------------------------
-         URL resource = getClass().getResource("../view/" + location + ".fxml");
-         Parent load = FXMLLoader.load(resource);
-         anchorPane.getChildren().clear();
-         anchorPane.getChildren().add(load);
-         */
-
         anchorPane.getChildren().clear();
-        anchorPane.getChildren().add(FXMLLoader.load(UILoader.class.getResource("../view/" + location + ".fxml")));
+        anchorPane.getChildren().add(FXMLLoader.load(UILoader.class.getResource("/view/" + location + ".fxml")));
     }
 
     public static void loadUiHalfDashBoard(AnchorPane anchorPane, String location) throws IOException {
-
-        /**----------------------------------FULL CODE------------------------------------------
-         URL resource = getClass().getResource("../view/" + location + ".fxml");
-         Parent load = FXMLLoader.load(resource);
-         anchorPane.getChildren().clear();
-         anchorPane.getChildren().add(load);
-         */
-
         anchorPane.getChildren().clear();
-        anchorPane.getChildren().add(FXMLLoader.load(UILoader.class.getResource("../view/" + location + ".fxml")));
+        anchorPane.getChildren().add(FXMLLoader.load(UILoader.class.getResource("/view/" + location + ".fxml")));
     }
 
     public static void BtnLogOut(AnchorPane anchorPane, String location) throws IOException {
-
-        /**----------------------------------FULL CODE------------------------------------------
-         URL resource = UILoader.class.getResource("/view/" + location + ".fxml");
-         Parent load = FXMLLoader.load(resource);
-         Stage window = (Stage) anchorPane.getScene().getWindow();
-         window.setScene(new Scene(load));
-         */
-
         Stage stage = (Stage) anchorPane.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(UILoader.class.getResource("../view/" + location + ".fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(UILoader.class.getResource("/view/" + location + ".fxml"))));
         stage.centerOnScreen();
     }
 
     public static void aboutOnAction(String location) throws IOException {
-
-        /**----------------------------------FULL CODE------------------------------------------
-         URL resource = getClass().getResource("../view/" + location + ".fxml");
-         Parent load = FXMLLoader.load(resource);
-         Stage stage = new Stage();
-         stage.setScene(new Scene(load));
-         stage.centerOnScreen();
-         stage.setResizable(true);
-         stage.show();
-         */
-
         Stage stage = new Stage();
-        stage.setScene(new Scene(FXMLLoader.load(UILoader.class.getResource("../view/" + location + ".fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(UILoader.class.getResource("/view/" + location + ".fxml"))));
         stage.centerOnScreen();
         stage.setResizable(true);
         stage.initStyle(StageStyle.UNDECORATED);
@@ -77,13 +42,6 @@ public class UILoader {
     }
 
     public static void LoginOnAction(AnchorPane anchorPane, String location) throws IOException, SQLException {
-        /**----------------------------------FULL CODE------------------------------------------
-         URL resource = UILoader.class.getResource("/view/" + location + ".fxml");
-         Parent load = FXMLLoader.load(resource);
-         Stage window = (Stage) anchorPane.getScene().getWindow();
-         window.setScene(new Scene(load));
-         */
-
         Stage window = (Stage) anchorPane.getScene().getWindow();
         window.setScene(new Scene(FXMLLoader.load(UILoader.class.getResource("/view/" + location + ".fxml"))));
     }
