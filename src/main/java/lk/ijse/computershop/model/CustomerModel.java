01c31b9c03473fa5f36967dd456ef937f9fada9e
Thushal2001 <kavithmathushal9007@gmail.com>
2023-04-17 12:23:48 +0530

    Initial commit

--- src/main/java/lk/ijse/computershop/model/CustomerModel.java
+++ src/main/java/lk/ijse/computershop/model/CustomerModel.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class CustomerModel {
 
     public static int save(Customer customer) throws SQLException {
-        String sql = "INSERT INTO customers VALUES (?,?,?,?,?,?)";
+        String sql = "INSERT INTO customer VALUES (?,?,?,?,?,?)";
 
         return CrudUtil.execute(
                 sql,
@@ -26,7 +26,7 @@ public class CustomerModel {
 
     public static Customer search(String id) throws SQLException {
 
-        String sql = "SELECT * FROM customers WHERE id=?";
+        String sql = "SELECT * FROM customer WHERE id=?";
 
         ResultSet resultSet = CrudUtil.execute(sql, id);
 
@@ -45,7 +45,7 @@ public class CustomerModel {
 
     public static int update(Customer customer) throws SQLException {
 
-        String sql = "UPDATE customers SET name=? , nic=? , email=? , contact=? , address=? WHERE id=?";
+        String sql = "UPDATE customer SET name=? , nic=? , email=? , contact=? , address=? WHERE id=?";
 
         return CrudUtil.execute(
                 sql,
@@ -59,14 +59,14 @@ public class CustomerModel {
     }
 
     public static int delete(String id) throws SQLException {
-        String sql = "DELETE FROM customers WHERE id=?";
+        String sql = "DELETE FROM customer WHERE id=?";
         return CrudUtil.execute(sql, id);
     }
 
     public static List<Customer> getAll() throws SQLException {
 
         List<Customer> customerList = new ArrayList<>();
-        String sql = "SELECT * FROM customers";
+        String sql = "SELECT * FROM customer";
         ResultSet resultSet = CrudUtil.execute(sql);
 
         while (resultSet.next()) {
@@ -84,7 +84,7 @@ public class CustomerModel {
     }
 
     public static String getNextCustomerId() throws SQLException {
-        String sql = "SELECT id FROM customers ORDER BY id DESC LIMIT 1";
+        String sql = "SELECT id FROM customer ORDER BY id DESC LIMIT 1";
         ResultSet resultSet = CrudUtil.execute(sql);
 
         if (resultSet.next()) {
@@ -104,7 +104,7 @@ public class CustomerModel {
     }
 
     public static List<String> loadIds() throws SQLException {
-        String sql = "SELECT id FROM customers";
+        String sql = "SELECT id FROM customer";
         ResultSet resultSet = CrudUtil.execute(sql);
 
         List<String> data = new ArrayList<>();
@@ -115,7 +115,7 @@ public class CustomerModel {
     }
 
     public static Customer searchById(String customerId) throws SQLException {
-        String sql = "SELECT * FROM Customers WHERE id = ?";
+        String sql = "SELECT * FROM Customer WHERE id = ?";
         ResultSet resultSet = CrudUtil.execute(sql, customerId);
 
         if (resultSet.next()) {
