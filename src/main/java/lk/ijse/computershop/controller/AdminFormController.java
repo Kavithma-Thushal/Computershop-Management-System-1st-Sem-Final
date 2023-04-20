839a688c7b6844f8bc166b7cd7235ff1ae136b9a
Thushal2001 <kavithmathushal9007@gmail.com>
2023-04-20 13:03:45 +0530

    Initial commit

--- src/main/java/lk/ijse/computershop/controller/AdminFormController.java
+++ src/main/java/lk/ijse/computershop/controller/AdminFormController.java
@@ -1,12 +1,17 @@
 package lk.ijse.computershop.controller;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 import lk.ijse.computershop.util.UILoader;
+import org.controlsfx.control.Notifications;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -23,7 +28,17 @@ public class AdminFormController {
     @FXML
     private void adminLoginOnAction(MouseEvent mouseEvent) throws IOException, SQLException {
         if (txtUsername.getText().equals("") && txtPassword.getText().equals("")) {
-            UILoader.LoginOnAction(root,"admindashboard_form");
+            UILoader.LoginOnAction(root, "admindashboard_form");
+
+            Image img = new Image("/assets/icons/tick.gif", 90, 90, false, false);
+            Notifications notificationBuilder = Notifications.create()
+                    .title("Admin login ")
+                    .text("Admin login Successful")
+                    .graphic(new ImageView(img))
+                    .hideAfter(Duration.seconds(5))
+                    .position(Pos.BOTTOM_RIGHT);
+            notificationBuilder.darkStyle();
+            notificationBuilder.show();
         } else {
             new Alert(Alert.AlertType.ERROR, "please try again...!").show();
         }
--- src/main/java/lk/ijse/computershop/controller/CashierFormController.java
+++ src/main/java/lk/ijse/computershop/controller/CashierFormController.java
@@ -1,12 +1,17 @@
 package lk.ijse.computershop.controller;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 import lk.ijse.computershop.util.UILoader;
+import org.controlsfx.control.Notifications;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -24,6 +29,16 @@ public class CashierFormController {
     private void cashierLoginOnAction(MouseEvent mouseEvent) throws IOException, SQLException {
         if (txtUsername.getText().equals("") && txtPassword.getText().equals("")) {
             UILoader.LoginOnAction(root, "cashierdashboard_form");
+
+            Image img = new Image("/assets/icons/tick.gif", 90, 90, false, false);
+            Notifications notificationBuilder = Notifications.create()
+                    .title("Cashier login ")
+                    .text("Cashier login Successful")
+                    .graphic(new ImageView(img))
+                    .hideAfter(Duration.seconds(5))
+                    .position(Pos.BOTTOM_RIGHT);
+            notificationBuilder.darkStyle();
+            notificationBuilder.show();
         } else {
             new Alert(Alert.AlertType.ERROR, "please try again...!").show();
         }
