060d69686f9fb940dfba712a2a6e37b4006b4563
Thushal2001 <kavithmathushal9007@gmail.com>
2023-04-25 15:24:32 +0530

    Initial commit

--- src/main/java/lk/ijse/computershop/controller/ManageitemFormController.java
+++ src/main/java/lk/ijse/computershop/controller/ManageitemFormController.java
@@ -51,8 +51,8 @@ public class ManageitemFormController implements Initializable {
     private TextField txtSearch;
 
     private LinkedHashMap<TextField, Pattern> map = new LinkedHashMap();
-    Pattern description = Pattern.compile("^([A-Z a-z 0-9]{4,40})$");
-    Pattern unitPrice = Pattern.compile("^(?!00)[0-9]{2,6}(?:\\.[0-9]{1,2})?$");
+    Pattern description = Pattern.compile("^([A-Z a-z 0-9 \\W]{4,40})$");
+    Pattern unitPrice = Pattern.compile("^(?!00)[0-9]{2,8}(?:\\.[0-9]{2})?$");
     Pattern qtyOnHand = Pattern.compile("^([0-9]{1,6})$");
 
     @Override
