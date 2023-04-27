20259a75d0abdff475260690f37322366190df6e
Thushal2001 <kavithmathushal9007@gmail.com>
2023-04-27 21:36:43 +0530

    Initial commit

--- src/main/java/lk/ijse/computershop/controller/ManagereportsFormController.java
+++ src/main/java/lk/ijse/computershop/controller/ManagereportsFormController.java
@@ -1,7 +1,7 @@
 package lk.ijse.computershop.controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import lk.ijse.computershop.db.DBConnection;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.view.JasperViewer;
@@ -13,15 +13,12 @@ import java.util.HashMap;
 public class ManagereportsFormController {
 
     @FXML
-    private void customerReportOnAction(ActionEvent event) throws SQLException, JRException {
+    private void customerReport(MouseEvent mouseEvent) throws JRException, SQLException {
         HashMap<String, Object> map = new HashMap<>();
         map.put("Customer", "Thushal");
-        //map.put("Table Name",txt.getText());
-
         InputStream resource = this.getClass().getResourceAsStream("/reports/customerReport.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(resource);
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, map, DBConnection.getInstance().getConnection());
-        //JasperPrintManager.printReport(jasperPrint,true);
         JasperViewer.viewReport(jasperPrint, false);
     }
 }
