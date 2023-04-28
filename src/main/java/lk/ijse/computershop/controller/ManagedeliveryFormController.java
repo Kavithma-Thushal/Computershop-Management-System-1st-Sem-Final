25ddc255f743ef9472230b723e248a0ce8639933
Thushal2001 <kavithmathushal9007@gmail.com>
2023-04-28 20:57:38 +0530

    Initial commit

--- src/main/java/lk/ijse/computershop/controller/ManagedeliveryFormController.java
+++ src/main/java/lk/ijse/computershop/controller/ManagedeliveryFormController.java
@@ -7,16 +7,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import lk.ijse.computershop.dto.*;
 import lk.ijse.computershop.dto.tm.DeliveryTM;
 import lk.ijse.computershop.model.*;
 import lk.ijse.computershop.util.CrudUtil;
+import lk.ijse.computershop.util.Validation;
 
 import java.net.URL;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 public class ManagedeliveryFormController implements Initializable {
 
@@ -52,8 +57,13 @@ public class ManagedeliveryFormController implements Initializable {
     private TableColumn colLocation;
     @FXML
     private TableColumn colDate;
+    @FXML
+    private Button btnDeliver;
 
     private ObservableList<DeliveryTM> observableList = FXCollections.observableArrayList();
+
+    private LinkedHashMap<TextField, Pattern> map = new LinkedHashMap();
+    Pattern location = Pattern.compile("^([A-Z a-z]{4,40})$");
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -64,6 +74,8 @@ public class ManagedeliveryFormController implements Initializable {
         loadCustomerIds();
         loadEmployeeIds();
         loadOrderIds();
+        storeValidations();
+        btnDeliver.setDisable(true);
     }
 
     private void setCellValueFactory() {
@@ -73,6 +85,38 @@ public class ManagedeliveryFormController implements Initializable {
         colOrderId.setCellValueFactory(new PropertyValueFactory<>("orderId"));
         colLocation.setCellValueFactory(new PropertyValueFactory<>("location"));
         colDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+    }
+
+    private void storeValidations() {
+        map.put(txtLocation, location);
+    }
+
+    private void clearAllTxt() {
+        txtCustomerName.clear();
+        txtEmployeeName.clear();
+        txtOrderDescription.clear();
+        txtLocation.clear();
+
+        btnDeliver.setDisable(true);
+        setBorders(txtLocation);
+    }
+
+    public void setBorders(TextField... textFields) {
+        for (TextField textField : textFields) {
+            textField.setStyle("-fx-border-color: transparent");
+        }
+    }
+
+    @FXML
+    private void txtKeyRelease(KeyEvent keyEvent) {
+        Object response = Validation.validate(map, btnDeliver);
+
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            if (response instanceof TextField) {
+                TextField txtnext = (TextField) response;
+                txtnext.requestFocus();
+            }
+        }
     }
 
     private void getAll() {
@@ -128,7 +172,7 @@ public class ManagedeliveryFormController implements Initializable {
     private void loadEmployeeIds() {
         try {
             ObservableList<String> observableList = FXCollections.observableArrayList();
-            List<String> employeeId = EmployeeModel.loadIds();
+            List<String> employeeId = EmployeeModel.loadEmployeeIds();
 
             for (String id : employeeId) {
                 observableList.add(id);
@@ -193,16 +237,20 @@ public class ManagedeliveryFormController implements Initializable {
         String location = txtLocation.getText();
 
         try {
-            String sql = "INSERT INTO delivery VALUES(?, ?, ?, ?, ?, ?)";
-            int affectedRows = CrudUtil.execute(sql, deliveryCode, customerId, employeeId, orderId, location, String.valueOf(LocalDate.now()));
-            if (affectedRows > 0) {
-                new Alert(Alert.AlertType.INFORMATION, "Added Successfully...!").show();
-                tblDelivery.refresh();
-                getAll();
+            if (!txtCustomerName.getText().isEmpty() && !txtEmployeeName.getText().isEmpty() && !txtLocation.getText().isEmpty()) {
+                String sql = "INSERT INTO delivery VALUES(?, ?, ?, ?, ?, ?)";
+                int affectedRows = CrudUtil.execute(sql, deliveryCode, customerId, employeeId, orderId, location, String.valueOf(LocalDate.now()));
+                if (affectedRows > 0) {
+                    new Alert(Alert.AlertType.INFORMATION, "Added Successfully...!").show();
+                    getAll();
+                    clearAllTxt();
+                    generateNextDeliveryCode();
+                }
+            } else {
+                new Alert(Alert.AlertType.ERROR, "please try again...!").show();
             }
         } catch (Exception e) {
             new Alert(Alert.AlertType.ERROR, "please try again...!").show();
         }
-        generateNextDeliveryCode();
     }
 }
